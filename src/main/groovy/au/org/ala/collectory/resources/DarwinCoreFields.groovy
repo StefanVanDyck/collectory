1323737464b8363708d982456faf2b22140281b6
kshaleninova <katya@gleveconsult.com>
2026-02-03 09:24:36 +0100

    inbo/vlaams-biodiversiteitsportaal#1 - added 'EbirdValidated' value to identificationVerificationStatus

--- src/main/groovy/au/org/ala/collectory/resources/DarwinCoreFields.groovy
+++ src/main/groovy/au/org/ala/collectory/resources/DarwinCoreFields.groovy
@@ -37,7 +37,7 @@ class DarwinCoreFields {
             new DarwinCoreField(name: 'identifiedBy', important: true),
             new DarwinCoreField(name: 'identificationReferences', important: true),
             new DarwinCoreField(name: 'identificationQualifier', important: true),
-            new DarwinCoreField(name: 'identificationVerificationStatus',  values: ["","verified", "unverified", "validated", "INaturalistValidated"], important: true),
+            new DarwinCoreField(name: 'identificationVerificationStatus',  values: ["","verified", "unverified", "validated", "INaturalistValidated", "EbirdValidated"], important: true),
             new DarwinCoreField(name: 'kingdom', important: true),
             new DarwinCoreField(name: 'taxonRank', values: ["","kingdom","subkingdom","division","phylum","subdivision","subphylum","class","subclass","order","suborder","family","subfamily","tribe","subtribe","genus","subgenus","section","subsection","series","subseries","species","subspecies","variety","subvariety","form","subform"
             ], important: true),
