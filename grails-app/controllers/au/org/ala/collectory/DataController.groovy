5d24fdf6eeeea189321852786d1d157c93335e3e
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-05-11 14:44:15 +0200

    Allow only admins to download raw archives

--- grails-app/controllers/au/org/ala/collectory/DataController.groovy
+++ grails-app/controllers/au/org/ala/collectory/DataController.groovy
@@ -403,7 +403,10 @@ class DataController {
     def fileDownload = {
         String[] requiredRoles = [grailsApplication.config.ROLE_ADMIN]
         String[] requiredScopes = [grailsApplication.config.REQUIRED_SCOPES]
-        authCheck =  collectoryAuthService.isAuthorised(requiredRoles,requiredScopes)
+        if (!collectoryAuthService.isAuthorised(requiredRoles,requiredScopes)) {
+          unauthorised()
+          return
+        }
 
         def dirpath = "/" + params.directory + "/"
         def idx = request.forwardURI.lastIndexOf(dirpath) + dirpath.length()
